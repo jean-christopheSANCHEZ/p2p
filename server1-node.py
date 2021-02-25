e81edddea546eed225b5fb2028341a85a2001777
jean-christopheSANCHEZ <34402394+jean-christopheSANCHEZ@users.noreply.github.com>
2021-02-25 13:09:01 +0100

    Update server1-node.py

--- server1-node.py
+++ server1-node.py
@@ -13,10 +13,11 @@ log.setLevel(logging.DEBUG)
 
 loop_s1 = asyncio.get_event_loop()
 loop_s1.set_debug(True)
-
+print('coucou')
 server = Server()
-loop_s1.run_until_complete(server.listen(8468))
+loop_s1.run_until_complete(server.listen(5000))
 
+print(dir(server.bootstrap))
 try:
     loop_s1.run_forever()
 except KeyboardInterrupt:
